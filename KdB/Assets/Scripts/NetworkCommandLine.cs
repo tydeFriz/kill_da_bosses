a188b9f0eb59a8c7c99121da9ddfab5af321e7ed
unknown <friz3001@gmail.com>
2022-02-10 21:26:57 +0100

    foundation done

--- KdB/Assets/Scripts/NetworkCommandLine.cs
+++ KdB/Assets/Scripts/NetworkCommandLine.cs
@@ -1,18 +1,53 @@
-using System.Collections;
 using System.Collections.Generic;
+using MLAPI;
 using UnityEngine;
 
 public class NetworkCommandLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private NetworkManager netManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void Start()
+   {
+       netManager = GetComponentInParent<NetworkManager>();
+
+       if (Application.isEditor) return;
+
+       var args = GetCommandlineArgs();
+
+       if (args.TryGetValue("-mlapi", out string mlapiValue))
+       {
+           switch (mlapiValue)
+           {
+               case "server":
+                   netManager.StartServer();
+                   break;
+               case "host":
+                   netManager.StartHost();
+                   break;
+               case "client":
+                   netManager.StartClient();
+                   break;
+           }
+       }
+   }
+
+   private Dictionary<string, string> GetCommandlineArgs()
+   {
+       Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+       var args = System.Environment.GetCommandLineArgs();
+
+       for (int i = 0; i < args.Length; ++i)
+       {
+           var arg = args[i].ToLower();
+           if (arg.StartsWith("-"))
+           {
+               var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+               value = (value?.StartsWith("-") ?? false) ? null : value;
+
+               argDictionary.Add(arg, value);
+           }
+       }
+       return argDictionary;
+   }
 }
